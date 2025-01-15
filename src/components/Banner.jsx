92216f80b7387372e5d345929c2a4fcca6d49955
MountainSpiritq <greenbull122@gmail.com>
2025-01-15 14:01:49 +0100

    pizza forte changes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Banner = () => {
   return (
-    <div >
-   banner...
+    <div>
+   <h3>MI A PIZZAFORTNEL IMADUNK PIZZAKAT KESZITENI NEKED, NEKTEK, MINDENKINEK</h3>
   </div>
   )
 }
